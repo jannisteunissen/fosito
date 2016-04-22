e916bca9b34aa180562974ddb51ac186fcfb2d84
Jannis Teunissen <jannis@teunissen.net>
2016-04-22 12:38:44 +0200

    remove non-standard abort()

--- m_config.f90
+++ m_config.f90
@@ -75,8 +75,7 @@ contains
 
     print *, "The following fatal error occured in m_config:"
     print *, trim(err_string)
-    ! Gnu extension to get a backtrace
-    call abort()
+    stop
   end subroutine handle_error
 
   !> Update the variables in the configartion with the values found in 'filename'
